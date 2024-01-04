89137a89fed6fa15dc01e771a8816c61c948048c
Ifeanyi Mojekwu <107749742+Ifeanyi-M@users.noreply.github.com>
2024-01-04 16:53:15 +0100

    RabbitMQ implement

--- PeachTree.Services.AuthAPI/RabbitMQSender/RabbitMQAuthMessageSender.cs
+++ PeachTree.Services.AuthAPI/RabbitMQSender/RabbitMQAuthMessageSender.cs
@@ -20,23 +20,48 @@ namespace PeachTree.Services.AuthAPI.RabbitMQSender
         }
         public void SendMessage(object message, string queueName)
         {
-            var factory = new ConnectionFactory 
-            { 
-                HostName = _hostName,
-                Password = _password,
-                UserName = _username
-
-            };
-
-            _connection = factory.CreateConnection();
-
-            using var channel = _connection.CreateModel();
-            channel.QueueDeclare(queueName, false, false, false, null);
-            var json = JsonConvert.SerializeObject(message);
-            var body = Encoding.UTF8.GetBytes(json);
+            if (ConnectionExists())
+            {
+                using var channel = _connection.CreateModel();
+                channel.QueueDeclare(queueName, false, false, false, null);
+                var json = JsonConvert.SerializeObject(message);
+                var body = Encoding.UTF8.GetBytes(json);
 
 
-            channel.BasicPublish(exchange:"", routingKey:queueName,null, body:body);
+                channel.BasicPublish(exchange: "", routingKey: queueName, null, body: body);
+            }
+
+            
+        }
+
+        private void CreateConnection()
+        {
+            try
+            {
+                var factory = new ConnectionFactory
+                {
+                    HostName = _hostName,
+                    Password = _password,
+                    UserName = _username
+
+                };
+
+                _connection = factory.CreateConnection();
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+
+        private bool ConnectionExists()
+        {
+            if(_connection == null)
+            {
+                return true;
+            }
+            CreateConnection();
+            return true;
         }
     }
 }
